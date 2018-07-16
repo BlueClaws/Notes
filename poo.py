13b23951b7a550a4e007009fa921582fba5e3092
blueclaws <5661351+blueclaws@users.noreply.github.com>
2018-07-16 18:57:45 +0530

    Create poo.py

---
+++ poo.py
@@ -0,0 +1,17 @@
+import pai
+import pew
+
+class keeek(pai.poy, pew.pew):
+
+    def __init__(self):
+        pew.pew.__init__(self)
+        pai.poy.__init__(self)
+
+        self.kumer()
+
+    def kumer(self):
+        self.pooe()
+        self.pai()
+
+
+k = keeek()
